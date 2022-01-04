ca43afe6da41dbc0c55ffd24f07c94ebc2875245
simon <simon@dancingcloudservices.com>
2022-01-04 09:12:11 -0700

    Day 1 Break 1

---
+++ src/main/java/scope/Ex1.java
@@ -0,0 +1,32 @@
+package scope;
+
+public class Ex1 {
+  //  public static void main(String[] args) {
+//    {
+//      int x;
+//    }
+//    {
+//      int x;
+//    }
+//  }
+
+//  class X {
+//    /*void doStuff() */ {
+//      System.out.println(x); // works as this.x!!!
+//    }
+//    int x;
+//  }
+
+//  {
+//    int x;
+//    { int x; }
+//  }
+
+//  D) {
+//    int x;
+//    Predicate<String> ps = x -> true;
+//  }
+//  E) for (int x = 0; x < 10; x++)
+//  ;
+//  int x;
+}
